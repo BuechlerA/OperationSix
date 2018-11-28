168ce47767a92d566768385b4c35b78741916e1c
ibimsderalex <alexander.buechler25@gmail.com>
2018-11-28 13:49:47 +0100

    added new context menu

--- Assets/Scripts/Entities/HostageBase.cs
+++ Assets/Scripts/Entities/HostageBase.cs
@@ -1,8 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HostageBase : Entity
 {
-    
+
+    private Transform escortingOperator;
+    private NavMeshAgent myNavAgent;
+
+    private void Start()
+    {
+        myNavAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public override void Follow()
+    {
+        //base.Follow();
+        myNavAgent.SetDestination(escortingOperator.transform.position);
+    }
 }
