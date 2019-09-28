306d97cc037e34b5c33d33f82817dbaf3154085f
Alexander Büchler <alexander.buechler25@gmail.com>
2019-09-28 13:02:45 +0200

    added FPS system

---
+++ Assets/Scripts/FPS/InputController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public PlayerBehaviour playerBehaviour;
+
+    private Vector3 moveInput;
+    private Vector3 viewInput;
+
+    private bool inputEnabled = true;
+
+    public void Start()
+    {
+        if (playerBehaviour != null)
+        {
+            playerBehaviour.GetComponent<PlayerBehaviour>();
+        }
+
+        EnableInput();
+    }
+
+    public void Update()
+    {
+        if (inputEnabled)
+        {
+            float horizontalMove = Input.GetAxis("Horizontal");
+            float verticalMove = Input.GetAxis("Vertical");
+
+            float horizontalView = Input.GetAxisRaw("Mouse Y");
+            float verticalView = Input.GetAxisRaw("Mouse X");
+
+            moveInput = new Vector3(horizontalMove, 0, verticalMove);
+            viewInput = new Vector3(horizontalView, verticalView, 0);
+
+            if (inputEnabled && playerBehaviour != null)
+            {
+                playerBehaviour.GetInput(moveInput, viewInput);
+            }
+
+            if (Input.GetButton("Sprint"))
+            {
+                playerBehaviour.isRunning = true;
+            }
+            else
+            {
+                playerBehaviour.isRunning = false;
+            }
+        }
+    }
+
+    public void EnableInput()
+    {
+        inputEnabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
---
+++ Assets/Scripts/FPS/PlayerBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    [SerializeField]
+    private Camera cam;
+
+    private float currentSpeed;
+    public float walkSpeed = 2f;
+    public float runSpeed = 4f;
+
+    public float gravity = 20f;
+
+    public bool isRunning = false;
+
+    public void GetInput(Vector3 moveVector, Vector3 viewVector)
+    {
+        PlayerMove(moveVector);
+        PlayerView(viewVector);
+    }
+
+    public void PlayerMove(Vector3 moveVector)
+    {
+        if (isRunning)
+        {
+            currentSpeed = runSpeed;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+        characterController.Move(transform.TransformDirection(moveVector.x, 0, moveVector.z) * currentSpeed * Time.deltaTime);
+    }
+
+    public void PlayerView(Vector3 viewVector)
+    {
+        transform.localEulerAngles += new Vector3(0, viewVector.y, 0);
+        cam.transform.localEulerAngles += new Vector3(Mathf.Clamp(-viewVector.x, -80f, 80f), 0, 0);
+    }
+}
